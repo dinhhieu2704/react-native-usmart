f6175b8a24a38ec036cfeb3ad627c3884e176fd8
DINH-HIEU <kool.nguyen.zzz@gmail.com>
2024-02-24 21:32:35 +0700

    no message

--- App.tsx
+++ App.tsx
@@ -2,8 +2,6 @@ import { NavigationContainer } from "@react-navigation/native"
 import "react-native-gesture-handler"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { SafeAreaProvider } from "react-native-safe-area-context"
-import Welcome from "./app/screens/Welcome"
-import Navigation from "./app/navigation"
 import MyTab from "./app/components/tab/tab"
 
 export default function App() {
